30f7fc98975126be9d2704cd7c984bfc7ef61899
Mathew5693 <31830981+Mathew5693@users.noreply.github.com>
2020-02-10 10:30:39 -0500

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const MOVIELIST = require('./movies-data-small.json')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(cors())
+app.use(hemlet())
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
+  })
+
+app.get('/movie', function handleAvgVote(req, res){
+    let response = MOVIELIST;
+
+    if(req.query.avg_vote){
+        response = response.filter(average => average.avg_vote >= req.query.avg_vote)
+    }
+
+    if(req.query.country){
+        response = response.filter(country => country.country.toLowerCase() == req.query.country.toLowerCase())
+    }
+
+    if(req.query.genre){
+        response = response.filter(genre => genre.genre.toLowerCase() == req.query.genre.toLowerCase())
+    }
+
+
+    res.json(response)
+})
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    console.log(`server on port ${PORT}`)
+})
