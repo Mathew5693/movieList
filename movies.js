05198309a2b90cfac1acdf8c614854bedafb7aa2
Mathew5693 <31830981+Mathew5693@users.noreply.github.com>
2020-02-14 10:14:13 -0500

    Add files via upload

--- movies.js
+++ movies.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(morgan('dev'))
 app.use(cors())
-app.use(hemlet())
+app.use(helmet())
 
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
